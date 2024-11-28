d059db21a74e6f293a5fb1a55ea6e8b53a759e80
3030670 <3030670@30306-70>
2024-11-28 15:04:39 +0900

    初プッシュ

---
+++ PathValiableSample/src/main/java/com/example/demo/controller/PathValiableController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PathValiableController {
+	//GETかつ[url: /show]
+	@GetMapping("show")
+	public String showView() {
+		//戻り値は「ビュー名」を返す
+		return "show";
+	}
+
+	//GETかつ[/function/{no}]
+	//※{no}は動的に値が変わる
+	@GetMapping("/function/{no}")
+	//@PathVariablehがURLに埋め込まれた値{no}が格納される
+	public String selectFunction(@PathVariable Integer no) {
+		//「ビュー名」の初期化
+		String view = null;
+		switch(no) {
+		case 1:
+			view = "pathvaliable/function1";
+			break;
+		case 2:
+			view = "pathvaliable/function2";
+			break;
+		case 3:
+			view = "pathvaliable/function2";
+			break;
+		}
+
+		return view;
+	}
+
+
+	/** 「ボタンB」押下処理	 */
+	@PostMapping(value ="send" , params = "a")
+	public String showAview() {
+		//戻り値は「ビュー名」を返す
+		return "submit/a";
+	}
+
+	/** 「ボタンB」押下処理	 */
+	@PostMapping(value ="send", params ="b")
+	public String showBview() {
+		//戻り値は「ビュー名」を返す
+		return "submit/b";
+	}
+	
+	/** 「ボタンC」押下処理	 */
+	@PostMapping(value ="send", params ="c")
+	public String showCview() {
+		//戻り値は「ビュー名」を返す
+		return "submit/c";
+	}
+
+}
